f7d529f3dc9e57d0eefd0b23194530a4bc9d68a7
Austin Almond <austin226@live.com>
2016-02-10 11:16:39 -0500

    Clear up some language

--- main.c
+++ main.c
@@ -14,13 +14,13 @@
 
 /*
  * Performs a wavelet transform function on
- * a given input file. argv[1] is the filename,
+ * a given input signal. argv[1] is the input signal,
  * and argv[2] is the transform. Alternatively,
  * if these are not present, they will be
  * prompted inside main.
  */
 int main(int argc, char** argv) {
-	char wavelet_filename[ARG_LEN], transform_name[ARG_LEN];
+	char signal_name[ARG_LEN], transform_name[ARG_LEN];
 	/* TODO make sure args match ARG_LEN or less */
 
 	if (argc > 3) {
@@ -30,13 +30,13 @@ int main(int argc, char** argv) {
 		return (EXIT_SUCCESS);
 	}
 	else if (argc == 3) {
-		/* File and transform specified. */
-		strcpy(wavelet_filename, argv[1]);
+		/* Wavelet and transform specified. */
+		strcpy(signal_name, argv[1]);
 		strcpy(transform_name, argv[2]);
 	}
 	else if (argc == 2) {
-		/* File specified, no transform. */
-		strcpy(wavelet_filename, argv[1]);
+		/* Wavelet specified, no transform. */
+		strcpy(signal_name, argv[1]);
 
 		/* Prompt for tranform */
 		printf("Select transform: ");
@@ -46,15 +46,15 @@ int main(int argc, char** argv) {
 		/* Nothing specified. Prompt for info. */
 
 		/* Prompt for a wave file */
-		printf("Filename of wavelet data: ");
-		scanf("%s", &wavelet_filename);
+		printf("Name of signal: ");
+		scanf("%s", &signal_name);
 
 		/* Prompt for tranform */
 		printf("Select transform: ");
 		scanf("%s", &transform_name);
 	}
 
-	printf("file %s, trans %s\n", wavelet_filename, transform_name);
+	printf("signal %s, trans %s\n", signal_name, transform_name);
 
 
 	return (EXIT_SUCCESS);

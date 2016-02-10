184366afff69f2f71f25422b7c27829eb5861bf1
Austin Almond <austin226@live.com>
2016-02-10 10:47:34 -0500

    Link header to main.c

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ all:
 	mkdir -p ./lib
 	cd wavelib; make
 	cp wavelib/libwavelib.a lib/libwavelib.a
+	gcc -o wtdemo main.c lib/libwavelib.a
 
 clean:
 	cd wavelib; make clean
 	rm -f lib/*.a
+	rm ./wtdemo
--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: austin
@@ -14,10 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "wavelib/header/wavelib.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
+    puts("hey");
 
     return (EXIT_SUCCESS);
 }

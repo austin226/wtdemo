e1efb7a654b4501baca48da691ecd4d65c554c1b
Austin Almond <austin226@live.com>
2016-02-10 12:49:33 -0500

    Implemented test

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
  *
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,14 +13,18 @@
 
 #define ARG_LEN 100
 
-void do_wave_init(char *signal, char *wavelet) {
-	wave_object wo;
-	wo = wave_init(wavelet);
-	if (wo->filtlength < 0) {
-		printf("Invalid wavelet filter.\n");
-		return;
-	}
-	wave_summary(wo);
+double absmax(double *array, int N) {
+        double max;
+        int i;
+
+        max = 0.0;
+        for (i = 0; i < N; ++i) {
+                if (fabs(array[i]) >= max) {
+                        max = fabs(array[i]);
+                }
+        }
+
+        return max;
 }
 
 /*
@@ -32,7 +37,11 @@ int main(int argc, char** argv) {
 
 	char signal_name[ARG_LEN], wavelet_name[ARG_LEN], transform_name[ARG_LEN];
 
-	int N, J;
+        double *inp, *out, *diff;
+        int N, i, J;
+
+        FILE *ifp;
+        double temp[1200];
 
 	wave_object wo;
 	wt_object wt;
@@ -46,17 +55,38 @@ int main(int argc, char** argv) {
 	else {
 		/* TODO print usage */
 		printf("Need args\n");
-		return(EXIT_SUCCESS);
+		return(EXIT_FAILURE);
 	}
 
 	printf("signal %s, wavelet %s, trans %s\n", signal_name, wavelet_name, transform_name);
+
+        /* init signal */
+        ifp = fopen(signal_name, "r");
+        i = 0;
+        if (!ifp) {
+                printf("Can't open file.\n");
+                return (EXIT_FAILURE);
+        }
+        while (!feof(ifp)) {
+                fscanf(ifp, "%lf \n", &temp[i]);
+                i++;
+        }
+
+        inp = (double*)malloc(sizeof(double)* N);
+        out = (double*)malloc(sizeof(double)* N);
+        diff = (double*)malloc(sizeof(double)* N);
+
+        for (i = 0; i < N; ++i) {
+                inp[i] = temp[i];
+                //printf("%g \n",inp[i]);
+        }        
 
 	/* init wavelet */
 	wo = wave_init(wavelet_name);
 	if (wo->filtlength < 0) {
 		printf("Invalid wavelet filter.\n");
 		wave_free(wo);
-		return(EXIT_SUCCESS);
+		return(EXIT_FAILURE);
 	}
 	wave_summary(wo);
 	
@@ -65,6 +95,27 @@ int main(int argc, char** argv) {
 	wt = wt_init(wo, transform_name, N, J);
 	setDWTExtension(wt, "sym");
 	setWTConv(wt, "direct");
+
+        dwt(wt, inp);// Perform DWT
+        for (i = 0; i < wt->outlength; ++i) {
+                printf("%g ",wt->output[i]);
+        }
+
+        idwt(wt, out);// Perform IDWT (if needed)
+        // Test Reconstruction
+        for (i = 0; i < wt->siglength; ++i) {
+                diff[i] = out[i] - inp[i];
+        }
+
+        printf("\n MAX %g \n", absmax(diff, wt->siglength)); // If Reconstruction succeeded then the output should be a small value.
+        
+        wt_summary(wt);// Prints the full summary.
+        wave_free(wo);
+        wt_free(wt);
+
+        free(inp);
+        free(out);
+        free(diff);
 
 	return (EXIT_SUCCESS);
 }

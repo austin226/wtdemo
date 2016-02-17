5dde53cc5af4455ce6e80b7d73314c9be4849dcd
Austin Almond <austin226@live.com>
2016-02-17 13:06:44 -0500

    Remove wavepy, create 2d DWT file

---
+++ dwt2d.c
@@ -0,0 +1,9 @@
+/**
+ * Code for 2-D Discrete Wavelet transforms.
+ * Based on the algorithm detailed at http://www.mathworks.com/help/wavelet/ref/dwt2.html
+ * Integrates with Wavelib by Rafat Hussein.
+ *
+ * Author: Austin Almond
+ */
+
+/* TODO */
